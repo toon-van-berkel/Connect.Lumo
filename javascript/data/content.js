3c626f112510b07539aaeefba38ffa6f556c90f0
Toon van Berkel <toonvanberkel203@gmail.com>
2026-04-14 10:05:13 +0200

    Notification rework & Language switch (#2)
    * Connection system created
    
    The only thing left to add a function to connect the CPX to the website. Everything else is ready. Loading animations feedback and repeating reloads/retry.
    
    * Main Javascript code
    
    This is the main Javascript code that is imported in order to make the Notifications and the Connection work.
    
    * Created Notifications
    
    Notifications to give feedback to the user. Displaying warnings, information or just giving feedback on a successful action is possible. You can add timers and functions to these notifications making them really useful.
    
    * Styling for Connection + Navbar + Footer
    
    I made styling for the footer, navbar & the connection section.
    
    * Icons for notifications
    
    These are the icons that are being used in the notification script. These are self made and are meant to be clear and theme fitting.
    
    * Loading animation
    
    This is the loading animation that is visible when trying to connect.
    
    * Created the logo for Connect.Lumo
    
    This is the logo for Connect.Lumo. I kept the same style as for our banner.
    
    * Home page
    
    This is the new code for the homepage to make sure it works with the javascript and styling code.
    
    * Updated dates and changed div to a section
    
    * Created a .gitignore
    
    * Removed hardcoded html
    
    replacing hardcoded html with components
    
    * Added loading functions
    
    * Cleaning up connect code
    
    * Created content data for language switch
    
    * Code to generate components
    
    * Created a helper for console and future functions
    
    * User preferences loading and storing
    
    * Styling for notifications and the navbar & footer
    
    * Added function to close notification
    
    * Made type check more global
    
    * added feedback with notification
    
    * Notification styling

---
+++ javascript/data/content.js
@@ -0,0 +1,62 @@
+export let content = {
+    'en-gb': {
+        'nav': {
+            'link1':            'Home',
+            'link2':            'Controls',
+            'link3':            'Guide',
+            'imgAlt':           'Logo of Lumo connect',
+            'link4':            'Support',
+            'link5':            'Login'
+        },
+        'footer': {
+            'imgAlt':           'Logo of Lumo connect',
+            'link1':            'Controls',
+            'link2':            'Guide',
+            'link3':            'Contact',
+            'link4':            'Support',
+            'link5':            'Privacy',
+            'link6':            'General terms and conditions',
+            'link7':            'Cookies'
+        },
+        'connect': {
+            'connectButton':    'Connect Lumo',
+        },
+        'connectingComponent': {
+            'gifAlt':           'Loading animation',
+            'title':            'Connecting Lumo',
+            'message':          "We are connecting your Lumo, this might take a while. If it won't connect in 5 minutes ",
+            'link':             'make a ticket',
+            'badge':            'We are trying to connect you to our services.'
+        }
+    },
+    'nl-nl': {
+        'nav': {
+            'link1':            'Thuis',
+            'link2':            'Bediening',
+            'link3':            'Handleiding',
+            'imgAlt':           'Logo van Lumo connect',
+            'link4':            'Hulp',
+            'link5':            'Inloggen'
+        },
+        'footer': {
+            'imgAlt':           'Logo of Lumo connect',
+            'link1':            'Bediening',
+            'link2':            'Handleiding',
+            'link3':            'Contact',
+            'link4':            'Hulp',
+            'link5':            'Privacy',
+            'link6':            'Algemene voorwaarden',
+            'link7':            'Cookies'
+        },
+        'connect': {
+            'connectButton':    'Verbind Lumo',
+        },
+        'connectingComponent': {
+            'gifAlt':           'Verbinden animatie',
+            'title':            'Verbinden met Lumo',
+            'message':          "Wij zijn aan het verbinden met uw Lumo, dit kan even duren. Als het niet verbind binnen 5 minuten ",
+            'link':             'maak dan een ticket',
+            'badge':            'Wij proberen uw te verbinden met ons systeem.'
+        }
+    }
+};

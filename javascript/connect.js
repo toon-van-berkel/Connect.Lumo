27279eb213114fdf315b3805ae91331b2d6a81f5
Toon van Berkel <toonvanberkel203@gmail.com>
2026-04-15 11:45:05 +0200

    grammer errors + bugs + taal switch fixed
    I changed succes to success.
    I stopped the loading from bugging after a few tries.
    Fixed success notification styling.
    Implemented language switch into Notification.
    Added languages for deaf people.
    Added languages german + french.

--- javascript/connect.js
+++ javascript/connect.js
@@ -1,28 +1,55 @@
+// ============================================================================
+// ============================================================================
+// ============================= Import functions =============================
+// ============================================================================
+// ============================================================================
+import { content } from "./data/content.js";
+import { preferredLanguage } from "./preferences.js";
 import { addNotification } from './notification.js';
 import { generateConnectingComponent } from './generateContent.js';
 
+// ============================================================================
+// ============================================================================
+// ============================== Select elements =============================
+// ============================================================================
+// ============================================================================
 let notificationContainerElement = document.querySelector('#notificationContainer');
 let containerElement = document.querySelector('#container');
 
-let hasReloaded = false;
+// ============================================================================
+// ============================================================================
+// ================================= Variables ================================
+// ============================================================================
+// ============================================================================
 export let isLoading = false;
-
-let dots = '';
-
+let hasReloaded = false;
 let loadingInterval;
 let connectTimeout;
 let clearContainerTimeout;
 let loadingDotsElement;
+let dots = '';
+const CONNECT_TIMEOUT_MS = 5000;
+const RELOAD_COUNTDOWN_SECONDS = 20;
+const CLEAR_CONTAINER_DELAY_MS = RELOAD_COUNTDOWN_SECONDS * 1000;
 
+// ============================================================================
+// ============================================================================
+// =============== Load the section of the loading animation ==================
+// ============================================================================
+// ============================================================================
 function loadLoadingContent() {
     generateConnectingComponent();
     loadingDotsElement = document.querySelector('#load');
 }
 
+// ============================================================================
+// ============================================================================
+// ============== Load the animation of the dots next to title ================
+// ============================================================================
+// ============================================================================
 function startLoadingAnimation() {
     clearInterval(loadingInterval);
 
-    isLoading = true;
     dots = '';
 
     loadingInterval = setInterval(() => {
@@ -38,6 +65,11 @@ function startLoadingAnimation() {
     }, 500);
 }
 
+// ============================================================================
+// ============================================================================
+// ================ Stop the loading animation of the dots ====================
+// ============================================================================
+// ============================================================================
 function stopLoadingAnimation() {
     isLoading = false;
     clearInterval(loadingInterval);
@@ -47,32 +79,60 @@ function stopLoadingAnimation() {
     }
 }
 
+// ============================================================================
+// ============================================================================
+// =============== Place for the future real connecting code ==================
+// ============================================================================
+// ============================================================================
 function startConnecting() {
     clearTimeout(connectTimeout);
     clearTimeout(clearContainerTimeout);
 
     connectTimeout = setTimeout(() => {
-        addNotification('countingWarning', 'Unable to connect, attempting reload in', 20, Connect);
+        addNotification(
+            'countingWarning',
+            content[preferredLanguage].notifications.connectA,
+            RELOAD_COUNTDOWN_SECONDS,
+            connect
+        );
 
         clearContainerTimeout = setTimeout(() => {
             stopLoadingAnimation();
-            containerElement.innerHTML = '';
-        }, 19950);
-    }, 5000);
+
+            if (containerElement) {
+                containerElement.innerHTML = '';
+            }
+
+            clearContainerTimeout = null;
+        }, CLEAR_CONTAINER_DELAY_MS);
+
+        connectTimeout = null;
+    }, CONNECT_TIMEOUT_MS);
 }
 
+// ============================================================================
+// ============================================================================
+// ============= Function to load everything needed to connect ================
+// ============================================================================
+// ============================================================================
 export function connect() {
     clearInterval(loadingInterval);
     clearTimeout(connectTimeout);
     clearTimeout(clearContainerTimeout);
-
-    notificationContainerElement.innerHTML = '';
+    
+    if (notificationContainerElement) {
+        notificationContainerElement.innerHTML = '';
+    }
 
     if (hasReloaded) {
-        addNotification('Succes', 'Successfully reloaded');
+        console.log(content[preferredLanguage].notifications.connectB)
+        addNotification('success', content[preferredLanguage].notifications.connectB);
     }
 
     hasReloaded = true;
+    console.log(addNotification('information', content[preferredLanguage].notifications.connectC));
+
+    isLoading = true;
 
     loadLoadingContent();
     startLoadingAnimation();
--- javascript/data/content.js
+++ javascript/data/content.js
@@ -1,4 +1,9 @@
 export let content = {
+    // ============================================================================
+    // ============================================================================
+    // ================================== English =================================
+    // ============================================================================
+    // ============================================================================
     'en-gb': {
         'nav': {
             'link1':            'Home',
@@ -27,8 +32,57 @@ export let content = {
             'message':          "We are connecting your Lumo, this might take a while. If it won't connect in 5 minutes ",
             'link':             'make a ticket',
             'badge':            'We are trying to connect you to our services.'
+        },
+        'notifications': {
+            'connectA':        'Unable to connect, attempting reload in',
+            'connectB':        'Successfully reloaded',
+            'connectC':        'Connecting',
+            'preferencesA':    'Changed language to',
         }
     },
+    // English for deaf people
+    'en-gb-na': {
+        'nav': {
+            'link1':            'Home',
+            'link2':            'Buttons',
+            'link3':            'How it works',
+            'imgAlt':           'Lumo Connect logo',
+            'link4':            'Help',
+            'link5':            'Login'
+        },
+        'footer': {
+            'imgAlt':           'Lumo Connect logo',
+            'link1':            'Buttons',
+            'link2':            'How it works',
+            'link3':            'Contact',
+            'link4':            'Help',
+            'link5':            'Privacy',
+            'link6':            'Rules',
+            'link7':            'Cookies'
+        },
+        'connect': {
+            'connectButton':    'Connect Lumo',
+        },
+        'connectingComponent': {
+            'gifAlt':           'Waiting animation',
+            'title':            'Connect Lumo',
+            'message':          'Please wait. Lumo is connecting. Longer than 5 minutes? ',
+            'link':             'Ask for help',
+            'badge':            'We are connecting Lumo now.'
+        },
+        'notifications': {
+            'connectA':        'Cannot connect. Try again in',
+            'connectB':        'Reload done',
+            'connectC':        'Connecting',
+            'preferencesA':    'Language changed to',
+        }
+    },
+    
+    // ============================================================================
+    // ============================================================================
+    // =================================== Dutch ==================================
+    // ============================================================================
+    // ============================================================================
     'nl-nl': {
         'nav': {
             'link1':            'Thuis',
@@ -57,6 +111,207 @@ export let content = {
             'message':          "Wij zijn aan het verbinden met uw Lumo, dit kan even duren. Als het niet verbind binnen 5 minuten ",
             'link':             'maak dan een ticket',
             'badge':            'Wij proberen uw te verbinden met ons systeem.'
+        },
+        'notifications': {
+            'connectA':        'Kan niet verbinden, nieuwe poging over',
+            'connectB':        'Succesvol opnieuw geladen',
+            'connectC':        'Verbinden',
+            'preferencesA':    'Taal gewijzigd naar',
+        }
+    },
+    // Nederlands for deaf people
+    'nl-nl-na': {
+        'nav': {
+            'link1':            'Thuis',
+            'link2':            'Knoppen',
+            'link3':            'Uitleg',
+            'imgAlt':           'Logo van Lumo Connect',
+            'link4':            'Hulp',
+            'link5':            'Inloggen'
+        },
+        'footer': {
+            'imgAlt':           'Logo van Lumo Connect',
+            'link1':            'Knoppen',
+            'link2':            'Uitleg',
+            'link3':            'Contact',
+            'link4':            'Hulp',
+            'link5':            'Privacy',
+            'link6':            'Regels',
+            'link7':            'Cookies'
+        },
+        'connect': {
+            'connectButton':    'Verbind Lumo',
+        },
+        'connectingComponent': {
+            'gifAlt':           'Wacht animatie',
+            'title':            'Lumo verbinden',
+            'message':          'Even wachten. Lumo maakt verbinding. Duurt het langer dan 5 minuten? ',
+            'link':             'Vraag hulp',
+            'badge':            'Wij verbinden Lumo nu.'
+        },
+        'notifications': {
+            'connectA':        'Kan niet verbinden. Probeer opnieuw over',
+            'connectB':        'Opnieuw laden klaar',
+            'connectC':        'Verbinden',
+            'preferencesA':    'Taal veranderd naar',
+        }
+    },
+    
+    // ============================================================================
+    // ============================================================================
+    // ================================== German ==================================
+    // ============================================================================
+    // ============================================================================
+    'de-de': {
+        'nav': {
+            'link1':            'Startseite',
+            'link2':            'Steuerung',
+            'link3':            'Anleitung',
+            'imgAlt':           'Logo von Lumo Connect',
+            'link4':            'Support',
+            'link5':            'Anmelden'
+        },
+        'footer': {
+            'imgAlt':           'Logo von Lumo Connect',
+            'link1':            'Steuerung',
+            'link2':            'Anleitung',
+            'link3':            'Kontakt',
+            'link4':            'Support',
+            'link5':            'Datenschutz',
+            'link6':            'Allgemeine Geschäftsbedingungen',
+            'link7':            'Cookies'
+        },
+        'connect': {
+            'connectButton':    'Lumo verbinden',
+        },
+        'connectingComponent': {
+            'gifAlt':           'Ladeanimation',
+            'title':            'Mit Lumo verbinden',
+            'message':          'Wir verbinden Ihr Lumo. Das kann einen Moment dauern. Wenn es nach 5 Minuten nicht klappt ',
+            'link':             'erstellen Sie ein Ticket',
+            'badge':            'Wir verbinden Sie mit unseren Diensten.'
+        },
+        'notifications': {
+            'connectA':        'Verbindung nicht möglich, neuer Versuch in',
+            'connectB':        'Erfolgreich neu geladen',
+            'connectC':        'Verbinden',
+            'preferencesA':    'Sprache geändert zu',
+        }
+    },
+    // German for deaf people
+    'de-de-na': {
+        'nav': {
+            'link1':            'Start',
+            'link2':            'Knöpfe',
+            'link3':            'So geht es',
+            'imgAlt':           'Logo von Lumo Connect',
+            'link4':            'Hilfe',
+            'link5':            'Anmelden'
+        },
+        'footer': {
+            'imgAlt':           'Logo von Lumo Connect',
+            'link1':            'Knöpfe',
+            'link2':            'So geht es',
+            'link3':            'Kontakt',
+            'link4':            'Hilfe',
+            'link5':            'Datenschutz',
+            'link6':            'Regeln',
+            'link7':            'Cookies'
+        },
+        'connect': {
+            'connectButton':    'Lumo verbinden',
+        },
+        'connectingComponent': {
+            'gifAlt':           'Warte-Animation',
+            'title':            'Lumo verbinden',
+            'message':          'Bitte warten. Lumo verbindet. Dauert es länger als 5 Minuten? ',
+            'link':             'Hilfe fragen',
+            'badge':            'Wir verbinden Lumo jetzt.'
+        },
+        'notifications': {
+            'connectA':        'Keine Verbindung. Neuer Versuch in',
+            'connectB':        'Neu laden fertig',
+            'connectC':        'Verbinden',
+            'preferencesA':    'Sprache geändert zu',
+        }
+    },
+
+    // ============================================================================
+    // ============================================================================
+    // ================================== French ==================================
+    // ============================================================================
+    // ============================================================================
+    'fr-fr': {
+        'nav': {
+            'link1':            'Accueil',
+            'link2':            'Commandes',
+            'link3':            'Guide',
+            'imgAlt':           'Logo de Lumo Connect',
+            'link4':            'Assistance',
+            'link5':            'Connexion'
+        },
+        'footer': {
+            'imgAlt':           'Logo de Lumo Connect',
+            'link1':            'Commandes',
+            'link2':            'Guide',
+            'link3':            'Contact',
+            'link4':            'Assistance',
+            'link5':            'Confidentialité',
+            'link6':            'Conditions générales',
+            'link7':            'Cookies'
+        },
+        'connect': {
+            'connectButton':    'Connecter Lumo',
+        },
+        'connectingComponent': {
+            'gifAlt':           'Animation de chargement',
+            'title':            'Connexion à Lumo',
+            'message':          'Nous connectons votre Lumo. Cela peut prendre un moment. Si cela ne fonctionne pas après 5 minutes ',
+            'link':             'créez un ticket',
+            'badge':            'Nous essayons de vous connecter à nos services.'
+        },
+        'notifications': {
+            'connectA':        'Impossible de se connecter, nouvelle tentative dans',
+            'connectB':        'Rechargement réussi',
+            'connectC':        'Connexion',
+            'preferencesA':    'Langue changée en',
+        }
+    },
+    // French for deaf people
+    'fr-fr-na': {
+        'nav': {
+            'link1':            'Accueil',
+            'link2':            'Boutons',
+            'link3':            'Explication',
+            'imgAlt':           'Logo de Lumo Connect',
+            'link4':            'Aide',
+            'link5':            'Connexion'
+        },
+        'footer': {
+            'imgAlt':           'Logo de Lumo Connect',
+            'link1':            'Boutons',
+            'link2':            'Explication',
+            'link3':            'Contact',
+            'link4':            'Aide',
+            'link5':            'Confidentialité',
+            'link6':            'Règles',
+            'link7':            'Cookies'
+        },
+        'connect': {
+            'connectButton':    'Connecter Lumo',
+        },
+        'connectingComponent': {
+            'gifAlt':           'Animation attente',
+            'title':            'Connecter Lumo',
+            'message':          'Attendez. Lumo se connecte. Plus de 5 minutes ? ',
+            'link':             "Demander de l'aide",
+            'badge':            'Nous connectons Lumo maintenant.'
+        },
+        'notifications': {
+            'connectA':        'Pas de connexion. Nouvel essai dans',
+            'connectB':        'Rechargement terminé',
+            'connectC':        'Connexion',
+            'preferencesA':    'Langue changée en',
         }
     }
 };
